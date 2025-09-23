44686ac103d071112f92173d9e70045933133ed4
REVELLI ISSAK <isak88600@gmail.com>
2025-09-23 18:27:36 +0530

    50753

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuCategory(models.Model):
+    name = models.charfield(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+from rest_framework import serializers
+from .models import MenuCategory
+
+class Menucategoryserializer(serializers.modelserializer):
+    class meta:
+        model = Menucategoryserializer
+        fields = ['name']
+
+
+
+
