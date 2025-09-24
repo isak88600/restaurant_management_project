ad3cf2fb524b96f401ec1fbc5fdb836e4700836c
REVELLI ISSAK <isak88600@gmail.com>
2025-09-24 18:11:21 +0530

    50751

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from orders import pending, processing, completed,cancelled
 
 # Create your models here.
+from django.db import models
+class orderstatus(models.model):
+    name = models.charfield(max_lenght=50, unique=True)
+    def __str__(self):
+        return self.name
