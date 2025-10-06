2d0f2f6516f959c26c221928b0f8a58e975a0e4a
REVELLI ISSAK <isak88600@gmail.com>
2025-10-06 16:31:52 +0530

    52704

--- home/models.py
+++ home/models.py
@@ -1,10 +1,30 @@
-from django.db import models
-from .models import orderstatus
+from django.import models
+from django.contrib.auth.models import user
+
 
 class order(models.model):
+    user = models.foreignkey(user, on_delete=models.cascade, related)name = 'order')
+    date = models.datetimefield(auto_now_add=True)
+    total_price = models.decimalfield(max_digits=10, decimal_palce=2)
 
-    status = models.foreignkey(orderstatus, on_delete=models.set_null, null=True)
 
+class orderitem(models.model):
+    order = models.foreignkey(order, on_delete=model.cascade, related_name='items')
+    product_name = models.charfield(max_length=255)
+    quantity = models.positiveintegerfield()
+    price = models.decimalfield(max_digits=10, decimal_palce=2)
 
-    def __str__(self):
-        return f"order #{self.id} - self : {self.status}"
+from res_framework import serializers
+from. models order, orderitem
+
+class orderitemserializer(serializers.modelserializer):
+    class meta:
+        model = orderitem
+        fields = ['product_name', 'quanity', 'price']
+
+class orderserializer(serializers.modelserializer):
+    items = orderitemserializer(many=True, read_only=True)
+    class meta:
+        model = order
+        fields = ['id','date','total_price','item']
+
