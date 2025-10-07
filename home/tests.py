21cc61cc02c9d3a27095619be094d53a8a7245eb
REVELLI ISSAK <isak88600@gmail.com>
2025-10-07 17:36:53 +0530

    53866

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from datetime import datetime, time
+def is_restaurant_open():
+    """
+    Return True if the restaurant is currently open base on hardcoded
+    hourse otherwise returns False.
+    """
+    now = datetiime.now()
+    current_day = now.weekday()
+    current_time = now.time()
+
+    opening_hours = {
+        0: (time(9,0), time(22, 0)),
+        0: (time(9,0)),
+        0:
+    }
 
 # Create your tests here.
