b7789a3a74ef658f8a7da129767689ca73830214
REVELLI ISSAK <isak88600@gmail.com>
2025-10-08 17:47:25 +0530

    53864

--- account/models.py
+++ account/models.py
@@ -1,24 +1,16 @@
-from django.db import models
-class activeordermanager(models.manager):
-    def get_atctive_orders(self):
-        return self.get_queryset().filter(status_in=['pending','processng'])
-class order(models.model):
-    status_choices = [
-        ('pending','pending'),
-        ('processing','processing'),
-        ('completed','completed'),
-        ('cancelled','cancelled'),
-    ]
+from rest_framework import generics
+from home.models import table
+from home.serializers import tableserializer
 
-    staus = models.charfield(max_length=20, choices=status_choices)
-    create_at = models.datetimefield(auto_now_add=True)
+class tabledetailapiview(generics.retrieveapiview):
+    queryset = table.objects.all()
+    serializer_class = tableserializer
 
 
+from django.urls import path
+from home.views import tabledetailsapiview
 
-
-     objects = activeordermanager()
-
-     from orders.models import order
-
-     active_orders = order.objects.get_active_orders()
-     print(active_orders)
+urlpatterns = [
+    path('api/tables/<int:pk/', tabledetailsapiview.as_view(), name = 'table-detail'),
+    
+]
