833f8b06a5ff530c789cfae6baeb5ab98926bd1c
REVELLI ISSAK <isak88600@gmail.com>
2025-10-08 18:10:39 +0530

    53863

--- account/models.py
+++ account/models.py
@@ -1,16 +1,27 @@
-from rest_framework import generics
-from home.models import table
-from home.serializers import tableserializer
-
-class tabledetailapiview(generics.retrieveapiview):
-    queryset = table.objects.all()
-    serializer_class = tableserializer
+class category(models.model):
+    category_name = models. charfield(max_length=100)
 
 
-from django.urls import path
-from home.views import tabledetailsapiview
+class menuitems(models.model):
+    name = models.charfield(max_length=100)
+    price = models.decimalfield(max_digits=6, decimal_palces=2)
+    category -models.foreignkey(category, on_delete=models.cascade)
 
-urlpatterns = [
-    path('api/tables/<int:pk/', tabledetailsapiview.as_view(), name = 'table-detail'),
-    
-]
+    from rest_framework import serializers 
+    from .models import menuitems
+
+    class menuitemsserializers(serializers.modelserializer):
+        class meta:
+            model = menuitems
+            fields = '__all__'
+
+    from rest_framework. views import apiview
+    from rest_framework. response import response
+    from rest_framework import status
+    from. models import menuitems
+    from. serializers import menuitemsserializers
+
+    class menuitemsbycategoryapiview(apiview):
+        def get(self,request):
+            category_name = request.query_params.get('category')
+            if
